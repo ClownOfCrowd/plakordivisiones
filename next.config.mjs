6b9e1bb4cda39758df37189d5b11973d54661b52
ClownOfCrowd <anikanov.gleb@gmail.com>
2025-03-12 18:17:41 +0100

    fix: оптимизация для продакшн и добавление иконок

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,19 @@ const nextConfig = {
   skipMiddlewareUrlNormalize: true,
   experimental: {
     scrollRestoration: false
-  }
+  },
+  output: 'standalone',
+  images: {
+    domains: ['plakordivisiones.es'],
+    formats: ['image/avif', 'image/webp'],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+  },
+  poweredByHeader: false,
+  compress: true,
+  generateEtags: true,
+  reactStrictMode: true,
+  swcMinify: true
 };
 
 export default nextConfig; 
